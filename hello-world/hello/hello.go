9b6da9c906d4dfb501311e6a0360ce768e4c3c1b
André Eriksson <andre@encore.dev>
2021-08-30 11:18:10 +0200

    hello: improve Hello World example

--- hello-world/hello/hello.go
+++ hello-world/hello/hello.go
@@ -2,18 +2,50 @@ package hello
 
 import (
 	"context"
-	"fmt"
 )
 
-type WorldResponse struct {
-	// Message is the greeting response.
+// Welcome to Encore!
+// This is a simple "Hello World" project to get you started.
+//
+// To run it, execute "encore run" in your favorite shell.
+
+// ==================================================================
+
+// This is a simple REST API that responds with a personalized greeting.
+// To call it, run in your terminal:
+//
+//     curl http://localhost:4060/hello/World
+//
+//encore:api public path=/hello/:name
+func World(ctx context.Context, name string) (*Response, error) {
+	msg := "Hello, " + name + "!"
+	return &Response{Message: msg}, nil
+}
+
+type Response struct {
 	Message string
 }
 
-// World responds with a familiar message.
+// ==================================================================
+
+// Encore comes with a built-in development dashboard for
+// exploring your API, viewing documentation, debugging with
+// distributed tracing, and more. Visit your API URL in the browser:
 //
-// encore:api public
-func World(ctx context.Context) (*WorldResponse, error) {
-	msg := fmt.Sprintf("Hello, World!")
-	return &WorldResponse{Message: msg}, nil
-}
+//     http://localhost:4060
+//
+
+// ==================================================================
+
+// Next steps
+//
+// 1. Deploy your application to the cloud with a single command:
+//
+//     git push encore
+//
+// 2. To continue exploring Encore, check out one of these topics:
+//
+//    Building a Slack bot:  https://encore.dev/docs/tutorials/slack-bot
+//    Building a REST API:   https://encore.dev/docs/tutorials/rest-api
+//    Using SQL databases:   https://encore.dev/docs/develop/sql-database
+//    Authenticating users:  https://encore.dev/docs/develop/auth
--- hello-world/hello/hello_test.go
+++ hello-world/hello/hello_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestWorld(t *testing.T) {
-	resp, err := World(context.Background())
+	resp, err := World(context.Background(), "Jane Doe")
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := "Hello, World!"
+	want := "Hello, Jane Doe!"
 	if got := resp.Message; got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
