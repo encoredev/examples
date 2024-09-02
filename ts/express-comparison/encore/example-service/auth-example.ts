4d8d3342861a2ab6f4029f1d51e47c5ee9a66945
Simon Johansson <simon@encore.dev>
2024-09-02 13:53:19 +0200

    Add logging example to encore

--- ts/express-comparison/encore/example-service/auth-example.ts
+++ ts/express-comparison/encore/example-service/auth-example.ts
@@ -1,5 +1,6 @@
 import { api, APIError, Gateway, Header } from "encore.dev/api";
 import { authHandler } from "encore.dev/auth";
+import { getAuthData } from "~encore/auth";
 
 interface AuthParams {
   authorization: Header<"Authorization">;
@@ -7,7 +8,7 @@ interface AuthParams {
 
 // The function passed to authHandler will be called for all incoming API call that requires authentication.
 // https://encore.dev/docs/ts/develop/auth
-const myAuthHandler = authHandler(
+export const myAuthHandler = authHandler(
   async (params: AuthParams): Promise<{ userID: string }> => {
     // TODO: Validate up auth token and verify that this is an authenticated user
     const isInvalidUser = params.authorization === undefined;
@@ -26,7 +27,10 @@ export const gateway = new Gateway({ authHandler: myAuthHandler });
 export const dashboardEndpoint = api(
   // Setting auth to true will require the user to be authenticated
   { auth: true, method: "GET", path: "/dashboard" },
-  async (): Promise<{ message: string }> => {
-    return { message: "Secret dashboard message" };
+  async (): Promise<{ message: string; userID: string }> => {
+    return {
+      message: "Secret dashboard message",
+      userID: getAuthData()!.userID,
+    };
   },
 );
---
+++ ts/express-comparison/encore/example-service/logging-example.ts
@@ -0,0 +1,19 @@
+import { api } from "encore.dev/api";
+import log from "encore.dev/log";
+
+// Logging example
+// https://encore.dev/docs/ts/develop/logging
+export const loggingExample = api(
+  { expose: true, method: "GET", path: "/logging" },
+  async (): Promise<{ message: string }> => {
+    try {
+      // TODO: Fetch something from the DB
+    } catch (err) {
+      log.error(err, "something went terribly wrong!");
+    }
+
+    log.info("log message", { is_subscriber: true });
+
+    return { message: "Hello!" };
+  },
+);
