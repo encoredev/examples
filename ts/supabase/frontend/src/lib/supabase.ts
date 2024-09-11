5646589552fbb401956d408f2721ffe3a373e123
Leonardo Marciano <leonardo.marciano@toolzz.me>
2024-09-11 17:23:56 -0300

    chore: Add Encore Supabase example files and configurations

---
+++ ts/supabase/frontend/src/lib/supabase.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+        
