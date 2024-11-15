566dd99165afd76fbaaa78571147ec0d1a037b11
Simon Johansson <simon@encore.dev>
2024-11-15 14:19:51 +0100

    Fix tsconfig for next starter

--- ts/nextjs-starter/next.config.ts
+++ ts/nextjs-starter/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    tsconfigPath: "./tsconfig.vercel.json",
+  },
 };
 
 export default nextConfig;
