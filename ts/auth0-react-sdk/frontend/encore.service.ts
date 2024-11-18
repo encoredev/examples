30b70ee847d002efc8916d6808a0928d29fba3f1
Simon Johansson <mail@simon-johansson.com>
2024-11-18 13:53:10 +0100

    Update ts examples (#179)

--- ts/auth0-react-sdk/frontend/encore.service.ts
+++
@@ -1,5 +0,0 @@
-import { Service } from "encore.dev/service";
-
-// Encore will consider this directory and all its subdirectories as part of the "frontend" service.
-// https://encore.dev/docs/ts/primitives/services
-export default new Service("frontend");
--- ts/auth0-react-sdk/frontend/src/components/LoginStatus.tsx
+++ ts/auth0-react-sdk/frontend/src/components/LoginStatus.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 /**
