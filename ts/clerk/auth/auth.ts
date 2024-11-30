45927f09465690b004918ba066f3ca6b1fe7f491
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2024-11-30 23:45:16 +0530

    Update auth.ts

--- ts/clerk/auth/auth.ts
+++ ts/clerk/auth/auth.ts
@@ -1,13 +1,14 @@
-import {Clerk, verifyToken} from "@clerk/backend";
-import {APIError, Gateway, Header} from "encore.dev/api";
-import {authHandler} from "encore.dev/auth";
-import {secret} from "encore.dev/config";
-import {AUTHORIZED_PARTIES, DOMAIN} from "./config";
+import { createClerkClient, verifyToken } from "@clerk/backend";
+import { APIError, Gateway, Header } from "encore.dev/api";
+import { authHandler } from "encore.dev/auth";
+import { secret } from "encore.dev/config";
+
 import log from "encore.dev/log";
+import { AUTHORIZED_PARTIES } from "./config";
 
 const clerkSecretKey = secret("ClerkSecretKey");
 
-const clerkClient = Clerk({
+const clerkClient = createClerkClient({
   secretKey: clerkSecretKey(),
 });
 
@@ -21,32 +22,31 @@ interface AuthData {
   emailAddress: string | null;
 }
 
-const myAuthHandler = authHandler(
-  async (params: AuthParams): Promise<AuthData> => {
-    const token = params.authorization.replace('Bearer ', '');
+const myAuthHandler = authHandler(async (params: AuthParams): Promise<
+  AuthData
+> => {
+  const token = params.authorization.replace("Bearer ", "");
 
-    if (!token) {
-      throw APIError.unauthenticated("no token provided");
-    }
-
-    try {
-      const result = await verifyToken(token, {
-        issuer: DOMAIN,
-        authorizedParties: AUTHORIZED_PARTIES,
-      });
-
-      const user = await clerkClient.users.getUser(result.sub);
-
-      return {
-        userID: user.id,
-        imageUrl: user.imageUrl,
-        emailAddress: user.emailAddresses[0].emailAddress || null,
-      };
-    } catch (e) {
-      log.error(e);
-      throw APIError.unauthenticated("invalid token", e as Error);
-    }
+  if (!token) {
+    throw APIError.unauthenticated("no token provided");
   }
-);
 
-export const mygw = new Gateway({authHandler: myAuthHandler});
+  try {
+    const result = await verifyToken(token, {
+      authorizedParties: AUTHORIZED_PARTIES,
+    });
+
+    const user = await clerkClient.users.getUser(result.sub);
+
+    return {
+      userID: user.id,
+      imageUrl: user.imageUrl,
+      emailAddress: user.emailAddresses[0].emailAddress || null,
+    };
+  } catch (e) {
+    log.error(e);
+    throw APIError.unauthenticated("invalid token", e as Error);
+  }
+});
+
+export const mygw = new Gateway({ authHandler: myAuthHandler });
