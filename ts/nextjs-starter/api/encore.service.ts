4b572949c531217207b2b0f92134bc0cd0c9ed3f
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2025-01-23 17:40:14 +0700

    chore: fix comment typo

--- ts/nextjs-starter/api/encore.service.ts
+++ ts/nextjs-starter/api/encore.service.ts
@@ -1,5 +1,5 @@
 import { Service } from "encore.dev/service";
 
-// Encore will consider this directory and all its subdirectories as part of the "admin" service.
+// Encore will consider this directory and all its subdirectories as part of the "api" service.
 // https://encore.dev/docs/ts/primitives/services
 export default new Service("api");
