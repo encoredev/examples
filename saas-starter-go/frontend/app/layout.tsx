65892cfacd97963bdf3ccc97087a9277260e4521
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-02-28 17:32:35 +0100

    feat: start setting up clerk

--- saas-starter-go/frontend/app/layout.tsx
+++ saas-starter-go/frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs'
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
