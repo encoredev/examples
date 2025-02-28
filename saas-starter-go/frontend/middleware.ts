579666052167ed6d3923e4523ebbf0898fcb0abf
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-02-28 18:14:02 +0100

    feat: add sign in page

--- saas-starter-go/frontend/middleware.ts
+++ saas-starter-go/frontend/middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+});
 
 export const config = {
   matcher: [
