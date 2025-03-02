e1d8a459b563f3e671ac0a702e4d879952ab5035
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-03-02 19:46:51 +0100

    chore: re-add package

--- saas-starter-go/backend/auth/auth_handler.go
+++ saas-starter-go/backend/auth/auth_handler.go
@@ -2,8 +2,13 @@ package auth
 
 import (
 	"context"
+	"strings"
 
 	"encore.dev/beta/auth"
+	"encore.dev/beta/errs"
+	"github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/jwt"
+	"github.com/clerk/clerk-sdk-go/v2/user"
 )
 
 var secrets struct {
@@ -11,13 +16,13 @@ var secrets struct {
 }
 
 type UserData struct {
-	ID                    string  `json:"id"`
-	Username              *string `json:"username"`
-	FirstName             *string `json:"first_name"`
-	LastName              *string `json:"last_name"`
-	ImageURL              *string `json:"image_url"`
-	PrimaryEmailAddressID *string `json:"primary_email_address_id"`
-	// EmailAddresses        []*clerk.EmailAddress `json:"email_addresses"`
+	ID                    string                `json:"id"`
+	Username              *string               `json:"username"`
+	FirstName             *string               `json:"first_name"`
+	LastName              *string               `json:"last_name"`
+	ImageURL              *string               `json:"image_url"`
+	PrimaryEmailAddressID *string               `json:"primary_email_address_id"`
+	EmailAddresses        []*clerk.EmailAddress `json:"email_addresses"`
 }
 
 // The `encore:authhandler` annotation tells Encore to run this function for all
@@ -26,42 +31,37 @@ type UserData struct {
 //
 //encore:authhandler
 func AuthHandler(ctx context.Context, token string) (auth.UID, *UserData, error) {
-	return "user-id", &UserData{}, nil
-	/*
-	   clerk.SetKey(secrets.ClerkSecretKey)
+	clerk.SetKey(secrets.ClerkSecretKey)
 
-	   sessionToken := strings.TrimPrefix(token, "Bearer ")
+	sessionToken := strings.TrimPrefix(token, "Bearer ")
 
-	   	claims, err := jwt.Verify(ctx, &jwt.VerifyParams{
-	   		Token: sessionToken,
-	   	})
+	claims, err := jwt.Verify(ctx, &jwt.VerifyParams{
+		Token: sessionToken,
+	})
+	if err != nil {
+		return "", nil, &errs.Error{
+			Code:    errs.Unauthenticated,
+			Message: "invalid token",
+		}
+	}
 
-	   	if err != nil {
-	   		return "", nil, &errs.Error{
-	   			Code:    errs.Unauthenticated,
-	   			Message: "invalid token",
-	   		}
-	   	}
+	usr, err := user.Get(ctx, claims.Subject)
+	if err != nil {
+		return "", nil, &errs.Error{
+			Code:    errs.Unauthenticated,
+			Message: "could not fetch user",
+		}
+	}
 
-	   usr, err := user.Get(ctx, claims.Subject)
+	userData := &UserData{
+		ID:                    usr.ID,
+		Username:              usr.Username,
+		FirstName:             usr.FirstName,
+		LastName:              usr.LastName,
+		ImageURL:              usr.ImageURL,
+		PrimaryEmailAddressID: usr.PrimaryEmailAddressID,
+		EmailAddresses:        usr.EmailAddresses,
+	}
 
-	   	if err != nil {
-	   		return "", nil, &errs.Error{
-	   			Code:    errs.Unauthenticated,
-	   			Message: "could not fetch user",
-	   		}
-	   	}
-
-	   	userData := &UserData{
-	   		ID:                    usr.ID,
-	   		Username:              usr.Username,
-	   		FirstName:             usr.FirstName,
-	   		LastName:              usr.LastName,
-	   		ImageURL:              usr.ImageURL,
-	   		PrimaryEmailAddressID: usr.PrimaryEmailAddressID,
-	   		EmailAddresses:        usr.EmailAddresses,
-	   	}
-
-	   return auth.UID(usr.ID), userData, nil
-	*/
+	return auth.UID(usr.ID), userData, nil
 }
--- saas-starter-go/backend/go.mod
+++ saas-starter-go/backend/go.mod
@@ -12,8 +12,8 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-jose/go-jose/v3 v3.0.4 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.35.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
