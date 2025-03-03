c7bdf49d55a000570c8aae276d32aa7732227f2e
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-03-03 18:49:20 +0100

    fix: call new endpoint name

--- saas-starter-ts/frontend/app/dashboard/client-side-data.tsx
+++ saas-starter-ts/frontend/app/dashboard/client-side-data.tsx
@@ -1,32 +1,32 @@
 "use client";
 
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { useApiClient } from "@/lib/api/client-side";
 import { useQuery } from "@tanstack/react-query";
 
 export function ClientSideData() {
-	const apiClient = useApiClient();
+  const apiClient = useApiClient();
 
-	const { data } = useQuery({
-		queryKey: ["dashboardData"],
-		queryFn: () => apiClient.admin.GetDashboardData(),
-	});
+  const { data } = useQuery({
+    queryKey: ["dashboardData"],
+    queryFn: () => apiClient.admin.getDashboardData(),
+  });
 
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Client side data</CardTitle>
-				<CardDescription>
-					This data is fetched from the backend using client components
-				</CardDescription>
-			</CardHeader>
-			<CardContent>{JSON.stringify(data)}</CardContent>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Client side data</CardTitle>
+        <CardDescription>
+          This data is fetched from the backend using client components
+        </CardDescription>
+      </CardHeader>
+      <CardContent>{JSON.stringify(data)}</CardContent>
+    </Card>
+  );
 }
--- saas-starter-ts/frontend/app/dashboard/server-side-data.tsx
+++ saas-starter-ts/frontend/app/dashboard/server-side-data.tsx
@@ -1,25 +1,25 @@
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { getApiClient } from "@/lib/api/server-side";
 
 export async function ServerSideData() {
-	const apiClient = await getApiClient();
-	const data = await apiClient.admin.GetDashboardData();
+  const apiClient = await getApiClient();
+  const data = await apiClient.admin.getDashboardData();
 
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Server side data</CardTitle>
-				<CardDescription>
-					This data is fetched from the backend using server components
-				</CardDescription>
-			</CardHeader>
-			<CardContent>{JSON.stringify(data)}</CardContent>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Server side data</CardTitle>
+        <CardDescription>
+          This data is fetched from the backend using server components
+        </CardDescription>
+      </CardHeader>
+      <CardContent>{JSON.stringify(data)}</CardContent>
+    </Card>
+  );
 }
