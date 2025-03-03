c770d2fe117d602627783f3c9bf127d4ba355881
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-03-04 00:47:19 +0100

    feat: start adding support for organizations

--- saas-starter-ts/frontend/app/dashboard/app-sidebar.tsx
+++ saas-starter-ts/frontend/app/dashboard/app-sidebar.tsx
@@ -1,46 +1,51 @@
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarGroup,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { OrganizationSwitcher } from "@clerk/nextjs";
 import { Cog, LayoutDashboard, Wallet } from "lucide-react";
 import Link from "next/link";
 
 export function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
-	const { ...rest } = props;
+  const { ...rest } = props;
 
-	return (
-		<Sidebar {...rest}>
-			<SidebarContent>
-				<SidebarGroup>
-					<SidebarMenu>
-						<SidebarMenuItem>
-							<SidebarMenuButton asChild>
-								<Link href="/dashboard">
-									<LayoutDashboard /> Dashboard
-								</Link>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
-						<SidebarMenuItem>
-							<SidebarMenuButton asChild>
-								<Link href="/dashboard/subscription">
-									<Wallet /> Subscription
-								</Link>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
-						<SidebarMenuItem>
-							<SidebarMenuButton asChild>
-								<Link href="/dashboard/settings">
-									<Cog /> Settings
-								</Link>
-							</SidebarMenuButton>
-						</SidebarMenuItem>
-					</SidebarMenu>
-				</SidebarGroup>
-			</SidebarContent>
-		</Sidebar>
-	);
+  return (
+    <Sidebar {...rest}>
+      <SidebarHeader>
+        <OrganizationSwitcher hidePersonal />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href="/dashboard">
+                  <LayoutDashboard /> Dashboard
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href="/dashboard/subscription">
+                  <Wallet /> Subscription
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href="/dashboard/settings">
+                  <Cog /> Settings
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
 }
--- saas-starter-ts/frontend/app/dashboard/settings/page.tsx
+++ saas-starter-ts/frontend/app/dashboard/settings/page.tsx
@@ -1,7 +1,8 @@
 export default function SettingsPage() {
-	return (
-		<main className="container">
-			<h1>Settings</h1>
-		</main>
-	);
+  return (
+    <main className="container">
+      <h1>Settings</h1>
+
+    </main>
+  );
 }
